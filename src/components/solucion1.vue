461862a5ec0153e93e559c24c2932ed5dd3d1e78
wnicolas <nicolas.buitrago2gmail.com>
2020-12-20 23:04:30 -0500

    final servicios

--- src/components/solucion1.vue
+++ src/components/solucion1.vue
@@ -1,21 +1,189 @@
 <template>
-<div class="container-fluid">
+  <div class="container-fluid">
+    <h1 style="text-align: center">Horarios flexibles</h1>
 
-   <h1 style="text-align:center;">Horarios flexibles</h1>
-
+    <dl>
+      <dt>NORMATIVIDAD</dt>
+      <dd>
+        En Colombia el Código Sustantivo de Trabajo es el encargado de lograr la
+        justicia en las relaciones que surgen entre empleadores y trabajadores,
+        dentro del espíritu de coordinación económica y equilibrio social.
+        <br />
+        Dicho Código rige en todo el territorio de la República para todos sus
+        habitantes, sin consideración a su nacionalidad. <br />
+        El Código Sustantivo de Trabajo regula las relaciones de derecho
+        individual del Trabajo de carácter particular, y las de derecho
+        colectivo del Trabajo, oficiales y particulares.
+      </dd>
+      <dt>En cuanto al horario laboral flexible en Colombia</dt>
+      <dd>
+        <i
+          >El Código Sustantivo de trabajo en el Artículo 161, excepción d,
+          dispone:</i
+        >
+        <br />
+        <i
+          >"d) El empleador y el trabajador podrán acordar que la jornada
+          semanal de cuarenta y ocho (48) horas se realice mediante jornadas
+          diarias flexibles de trabajo, distribuidas en máximo seis días a la
+          semana con un día de descanso obligatorio, que podrá coincidir con el
+          domingo. Así, el número de horas de trabajo diario podrá repartirse de
+          manera variable durante la respectiva semana teniendo como mínimo
+          cuatro (4 horas) continuas y como máximo hasta diez (10) horas diarias
+          sin lugar a ningún recargo por trabajo suplementario, cuando el número
+          de horas de trabajo no exceda el promedio de cuarenta y ocho (48)
+          horas semanales dentro de la Jornada Ordinaria de 6 a.m. a 9 p.m.
+          <br />
+          <b>parágrafo</b> . El empleador no podrá, aún con el consentimiento
+          del trabajador, contratarlo para la ejecución de dos turnos en el
+          mismo día, salvo en labores de supervisión, dirección, confianza o
+          manejo.”
+        </i>
+      </dd>
+    </dl>
+    <h2>Modelo de horario fijo – variable</h2>
     <p>
-      laksjdlaskjd
+      Una alternativa para conseguir la armonía laboral satisfaciendo las
+      necesidades de los empleados de disponer de tiempo para acceder a sus
+      servicios de salud, estudio y otros aspectos personales; sin afectar su
+      rendimiento laboral y, por lo tanto, de la empresa, es ofrecer a los
+      empleados tres opciones de horarios laborales para que él escoja el que
+      considere más apropiado a sus necesidades y funciones. <br />
+
+      A continuación, se presentan las opciones de horarios laborales
+      implementadas en la oficina de abogados “Londoño & asociados” gracias a
+      nuestra asesoría. <br />
     </p>
-  
-</div>
+
+    <dl>
+      <h3>Opción 1</h3>
+      <table class="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Miercoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+            <th>Sábado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>6:00 a.m. – 12:00 p.m.</td>
+            <td>6:00 a.m. – 12:00 p.m.</td>
+            <td>6:00 a.m. – 12:00 p.m.</td>
+            <td>6:00 a.m. – 12:00 p.m.</td>
+            <td>8:00 a.m. - 12:00 p.m.</td>
+            <td>7:00 a.m. – 1:00 p.m.</td>
+          </tr>
+          <tr>
+            <td>2:00 p.m. – 6:00 p.m.</td>
+            <td>1:00 p.m. – 5 p.m.</td>
+            <td>2:00 p.m. – 6:00 p.m.</td>
+            <td>1:00 p.m. – 5 p.m.</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>10 Horas.</td>
+            <td>9 Horas.</td>
+            <td>10 Horas.</td>
+            <td>9 Horas.</td>
+            <td>4 Horas.</td>
+            <td>6 Horas.</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h3>Opción 2</h3>
+      <table class="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Miercoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+            <th>Sábado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>7:00 a.m. – 1:00 p.m.</td>
+            <td>6:00 a.m. – 12:00 p.m..</td>
+            <td>7:00 a.m. – 12:00 p.m..</td>
+            <td>7:00 a.m. – 12:00 p.m.</td>
+            <td>7:00 a.m. – 12:00 p.m.</td>
+            <td>8:00 a.m. – 12:00 p.m.</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>6:00 a.m. – 12:00 p.m.</td>
+            <td>1:00 p.m. – 5 p.m.</td>
+            <td>2:00 p.m. – 6:00 p.m.</td>
+            <td>1:00 p.m. – 5 p.m.</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>6 Horas.</td>
+            <td>10 Horas.</td>
+            <td>9 Horas.</td>
+            <td>10 Horas.</td>
+            <td>9 Horas.</td>
+            <td>4 Horas.</td>
+          </tr>
+        </tbody>
+      </table>
+
+      
+      <h3>Opción 3</h3>
+      <table class="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Miercoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+            <th>Sábado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>6:00 a.m. – 12:00 p.m.</td>
+            <td>8:00 a.m. – 12:00 p.m..</td>
+            <td>6:00 a.m. – 12:00 p.m..</td>
+            <td>7:00 a.m. – 12:00 p.m.</td>
+            <td>7:00 a.m. – 12:00 p.m.</td>
+            <td>6:00 a.m. – 12:00 p.m.</td>
+          </tr>
+          <tr>
+            <td>1:00 p.m. – 3:00 p.m</td>
+            <td>1:00 p.m. – 5 p.m.</td>
+            <td>2:00 p.m. – 6:00 p.m</td>
+            <td>1:00 p.m. – 4:00 p.m</td>
+            <td></td>
+            <td>1:00 p.m. – 3:00 p.m.</td>
+          </tr>
+          <tr>
+            <td>8 Horas.</td>
+            <td>8 Horas.</td>
+            <td>10 Horas.</td>
+            <td>8 Horas.</td>
+            <td>6 Horas.</td>
+            <td>8 Horas.</td>
+          </tr>
+        </tbody>
+      </table>
+    </dl>
+    <div class="alert alert-success">Los empleados de la oficina de abogados tuvieron la oportunidad de escoger el horario que más les convenia según sus funciones dentro de la oficina, así se les facilitó asistir a sus servicios médicos, de estudio y otros, sin necesidad de pedir permiso para ello y sin afectar su rendimiento en la oficina y por tanto el servicio que ofrece la oficina de abogados “Londoño & asociados”.</div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Solucion1',
-  props: {
-    
-  }
-}
+  name: "Solucion1",
+  props: {},
+};
 </script>
